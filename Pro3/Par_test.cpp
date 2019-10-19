30384a1ef107b42af7e71bb84c0717e550bd8f0c
eirikvw1 <42669645+eirikvw1@users.noreply.github.com>
2019-10-19 12:08:45 +0100

    Fikset var i c2 kan bruke den samme i d

---
+++ Pro3/Par_test.cpp
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <cstdio>
+int main (int argc, char *argv[]){
+int th_id, nthreads;
+#pragma omp parallel private(th_id) shared(nthreads)
+{
+th_id = omp_get_thread_num();
+printf("Hello World from thread %d\n", th_id);
+#pragma omp barrier
+if ( th_id == 0 ) {
+nthreads = omp_get_num_threads();
+printf("There are %d threads\n",nthreads);
+}
+}
+return 0;
+}
