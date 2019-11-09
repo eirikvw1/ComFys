aca2bf301736c73b69f91bfb9299b983a79571b3
eirikvw1 <42669645+eirikvw1@users.noreply.github.com>
2019-11-09 11:03:47 +0000

    Program almost don

---
+++ pro4/metroMain.h
@@ -0,0 +1,18 @@
+#ifndef metroMain
+#define metroMain
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include <armadillo>
+#include "metroAlg.h"
+#include <random>
+
+using namespace arma;
+using namespace  std;
+
+
+#endif
